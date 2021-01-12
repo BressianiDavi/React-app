c7045a29ffb5ea3bce46ca56fceac1eae39929e3
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-01-12 01:27:55 -0300

    Adicionei os componentes

--- companhia/src/App.js
+++ companhia/src/App.js
@@ -1,16 +1,25 @@
 import React, {Component} from "react";
+import {BrowserRouter, Route} from 'react-router-dom'
 
 import Site from './Site'
 import Rodape from './componentes/Rodape'
-
+import Cabecalho from './componentes/Cabecalho'
+import Inicio from './componentes/Inicio'
+import Servicos from './componentes/Servicos'
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div classname="App">
-          <Site/>
+          <Cabecalho/>
+          <Route path='/' exact component={Inicio} />
+
+          <Route path='/servicos' component={Servicos} />
+
           <Rodape/>
       </div>
+      </BrowserRouter>
     );
   }
 }
--- companhia/src/componentes/Rodape.js
+++ companhia/src/componentes/Rodape.js
@@ -1,19 +1,31 @@
-import React from 'react'
+/* eslint-disable react/jsx-no-target-blank */
+import React from "react";
 
-const Rodape = props => {
-    return (
-        <footer className='container-fluid text-center'>
-        <a href='#myPage' title='To Top'>
-          <span className='glyphicon glyphicon-chevron-up'></span>
+const Rodape = (props) => {
+  return (
+    <footer className="container-fluid text-center">
+      <a href="#myPage" title="To Top">
+        <span className="glyphicon glyphicon-chevron-up"></span>
+      </a>
+      <p>
+        <a
+          href="https://github.com/BressianiDavi"
+          target="_blank"
+          rel="external"
+          title="visit my gitHub">  
+          {" "}
+          Acesse o meu repositório no GitHub
         </a>
-        <p>
-            Meu primeiro site convertido de um template html para 100% React usando Bootstrap Theme Made By{' '}
-          <a href='https://www.w3schools.com' title='Visit w3schools'>
-            www.w3schools.com
-          </a>
-        </p>
-      </footer>
-    )
-}
+      </p>
+      <p>
+        Meu primeiro site convertido de um template html para 100% React usando
+        Bootstrap Theme Made By{" "}
+        <a href="https://www.w3schools.com" title="Visit w3schools">
+          www.w3schools.com
+        </a>
+      </p>
+    </footer>
+  );
+};
 
-export default Rodape
+export default Rodape;
