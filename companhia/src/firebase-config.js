73200a63967fc2a9e4ce6f74e7f9fa56a05a6318
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-01-29 20:36:49 -0300

    push

--- companhia/src/firebase-config.js
+++ companhia/src/firebase-config.js
@@ -1,7 +1,7 @@
 const Rebase = require("re-base");
-const firebase = require("firebase");
+var firebase = require('firebase/app');
 
-const config = {
+const firebaseConfig = {
   apiKey: "AIzaSyDlUYuL0Qo5h7NLMPwBxoGniy9zpYqiLAI",
   authDomain: "company-portfolio-63f13.firebaseapp.com",
   databaseURL: "https://company-portfolio-63f13-default-rtdb.firebaseio.com",
@@ -10,7 +10,9 @@ const config = {
   messagingSenderId: "191571735719",
   appId: "1:191571735719:web:9974f6d2c11b8905435782",
   measurementId: "G-SFE3YJXDXZ",
-}
-firebase.initializeApp(config)
+};
 
-export default config
+const app = firebase.initializeApp(firebaseConfig);
+const config = Rebase.createClass(app.database());
+
+export default config;
